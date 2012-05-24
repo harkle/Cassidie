753e5cbefc51a9d8df825133847872ca232b7feb
Lionel Tardy <net@lionel.me>
2012-05-24 14:52:27 +0200

    Some tests over npcs

--- server/samples/revival/characters/theTerribleNPC.js
+++ server/samples/revival/characters/theTerribleNPC.js
@@ -7,19 +7,29 @@ module.exports = NPC.extend({
 
 		this.attributes.name	= 'The terrible NPC'
 		this.attributes.skin 	= 7;
-		this.x					= 5;
-		this.y					= 5;
 		this.isEnnemy 			= true;
 
 		var self = this;
 		setTimeout(function() {
-			self.goLeft();
+			if (self.isMoving) {
+				self.moveTo(self.destinationX, self.destinationY, function() {
+					self.goRight();
+				})
+			} else {
+				if (self.x = 30 && self.y = 30) {
+					self.goRight();
+				} else {
+					self.x					= 5;
+					self.y					= 5;
+					self.goLeft();					
+				}
+			}
 		}, 1000);
 	},
 
 	goLeft: function() {
 		var self = this;
-		this.moveTo(10, 5, function() {
+		this.moveTo(30, 30, function() {
 			self.speak('Grrr! je suis méchant');
 			setTimeout(function() {
 				self.goRight();
